9cd2400c3dee5b5bd631d9b447f7e054a3ddabd5
Galibuu <josemi.gali4@gmail.com>
2026-03-17 08:50:18 -0500

    lele

--- src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import {Router} from "express";
-import {register, valLogin} from "../controllers/authController.js";
-
-const router = Router();
-
-router.post("/register", register);
-router.post("/login", valLogin);
-
-export default router
